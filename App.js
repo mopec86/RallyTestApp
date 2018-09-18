98317d8680fc9cda91d111d687d4be21459e723a
mopec86 <mopec86@gmail.com>
2018-09-18 19:14:11 +0000

    Last modifications before test in Windows Enviroment

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
+    items: { html:'My first rally App only prints this message (and one more in log).' },
     launch: function() {
         //Write app code here
-
+        console.log("First App Deployed In Rally-App-Builder created by JCMP");
         //API Docs: https://help.rallydev.com/apps/2.1/doc/
     }
 });
